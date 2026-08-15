93ea9e789f4e2b01b77fc1aafa265c4732b9bbcf
Josh Simmons <josh.simmons@gmail.com>
2026-08-15 16:18:02 +0900

    Two-line typing.

--- src/game.lua
+++ src/game.lua
@@ -1,6 +1,9 @@
-local text = "the quick brown fox jumped over the lazy dog"
-local left_cursor = 3
-local right_cursor = 6
+local left_text = "the quick brown fox jumped over the lazy dog"
+local right_text = "the quick brown fox jumped over the lazy dog"
+-- local left_text = "rearrests secret fever basseted crazes"
+-- local right_text = "polynyi junky jinni poilu jin um"
+local left_cursor = 2
+local right_cursor = 3
 local left_color = 1
 local right_color = 2
 local untyped_color = 3
@@ -12,29 +15,31 @@ end
 function _update()
   if peektext() then
     local char = readtext()
-    if sub(text, left_cursor, true) == char and left_cursor + 1 < right_cursor then
+    if sub(left_text, left_cursor, true) == char and left_cursor + 1 < right_cursor then
       left_cursor = left_cursor + 1
     end
-    if sub(text, right_cursor, true) == char and right_cursor - left_cursor < 6 then
+    if sub(right_text, right_cursor, true) == char and right_cursor - left_cursor < 6 then
       right_cursor = right_cursor + 1
     end
   end
 end
 
-function draw_cursor(cidx, col)
-  local x = (cidx - 1) * 5 - 1
-  line(x, 0, x, 8, col)
+function draw_cursor(cidx, x, y, col)
+  local x = (cidx - 1) * 5 - 1 + x
+  line(x, y, x, y + 8, col)
+end
+
+function draw_line(text, cursor, x, y, typed_col, untyped_col)
+  local typed_text = sub(text, 1, cursor - 1)
+  local untyped_text = sub(text, cursor)
+  local dx = print(typed_text, x, y, typed_col)
+  dx = print(untyped_text, dx, y, untyped_col)
+
+  draw_cursor(cursor, x, y, typed_col)
 end
 
 function _draw()
   cls()
-  local left_text = sub(text, 1, left_cursor - 1)
-  local right_text = sub(text, left_cursor, right_cursor - 1)
-  local untyped_text = sub(text, right_cursor)
-  local x = print(left_text, 0, 0, left_color)
-  x = print(right_text, x, 0, right_color)
-  x = print(untyped_text, x, 0, untyped_color)
-
-  draw_cursor(left_cursor, left_color)
-  draw_cursor(right_cursor, right_color)
+  draw_line(left_text, left_cursor, 0, 30, left_color, untyped_color)
+  draw_line(right_text, right_cursor, 0, 40, right_color, untyped_color)
 end
