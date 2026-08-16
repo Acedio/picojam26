22c6c0743e357d881cd81c421607214783a178f6
Josh Simmons <josh.simmons@gmail.com>
2026-08-16 11:58:22 +0900

    Making the text larger.

--- src/game.lua
+++ src/game.lua
@@ -6,23 +6,23 @@ press any key to start tutorial.]]
 
 local lines = {
   {
-    top=" move the front legs by typing on this line",
+    top="move the front legs by typing on this line",
     bot="move the back legs by typing on this line",
   },
   {
-    top=" get to the finish line",
+    top="get to the finish line",
     bot="without falling apart",
   },
   {
-    top=" today i had an apple for breakfast",
+    top="today i had an apple for breakfast",
     bot="gotta go fast today to win",
   },
   {
-    top=" the wind feels good on my face today",
+    top="the wind feels good on my face today",
     bot="if we win this race, we get a prize",
   },
   {
-    top=" A lovely bouquet!",
+    top="A lovely bouquet!",
     bot="A pretty bouquet!",
   },
 }
@@ -31,9 +31,9 @@ local stage = 1
 local bottom_text = lines[stage].bot
 local top_text = lines[stage].top
 local bottom_cursor = 1
-local top_cursor = 2
-local bottom_color = 1
-local top_color = 2
+local top_cursor = 1
+local bottom_color = 8
+local top_color = 9
 local untyped_color = 3
 
 function _init()
@@ -50,7 +50,7 @@ function _update()
       top_cursor = top_cursor + 1
       horse:set_front_hoof(top_cursor * 10)
     end
-    if sub(bottom_text, bottom_cursor, true) == char and bottom_cursor + 1 < top_cursor then
+    if sub(bottom_text, bottom_cursor, true) == char and bottom_cursor < top_cursor then
       bottom_cursor = bottom_cursor + 1
       horse:set_back_hoof(bottom_cursor * 10)
     end
@@ -59,15 +59,15 @@ function _update()
 end
 
 function draw_cursor(cidx, x, y, col)
-  local x = (cidx - 1) * 5 - 1 + x
-  line(x, y, x, y + 8, col)
+  local x = (cidx - 1) * 10 - 1 + x
+  line(x, y, x, y + 16, col)
 end
 
 function draw_line(text, cursor, x, y, typed_col, untyped_col)
   local typed_text = sub(text, 1, cursor - 1)
   local untyped_text = sub(text, cursor)
-  local dx = print(typed_text, x, y, typed_col)
-  dx = print(untyped_text, dx, y, untyped_col)
+  local dx = print("\^w\^t" .. typed_text, x, y, typed_col)
+  dx = print("\^w\^t" .. untyped_text, dx, y, untyped_col)
 
   draw_cursor(cursor, x, y, typed_col)
 end
@@ -75,7 +75,7 @@ end
 function _draw()
   cls()
   map()
-  draw_line(top_text, top_cursor, 10, 242, top_color, untyped_color)
+  draw_line(top_text, top_cursor, 10, 232, top_color, untyped_color)
   draw_line(bottom_text, bottom_cursor, 10, 252, bottom_color, untyped_color)
   horse:draw()
 end
