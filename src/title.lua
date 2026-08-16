3ca6e62c3a7cd9e001797b1d970af1125c450df0
Josh Simmons <josh.simmons@gmail.com>
2026-08-16 17:54:20 +0900

    he is perfect

--- src/title.lua
+++ src/title.lua
@@ -1,4 +1,5 @@
 local v2 = include("v2.lua")
+local Horse = include("horse.lua")
 
 include("texteffects.lua")
 
@@ -10,6 +11,8 @@ function Title:new()
   self.__index = self
   o.title = bubbletext("derpy derby", "\^w\^t", {x=nil, y=50})
   o.anykey = bubbletext("press any key!", "", {x=nil, y=170})
+  o.hormse = Horse:new{pos = v2.v2(0, 140)}
+  o.hormse_x = -80
   o:init()
   return o
 end
@@ -25,6 +28,14 @@ end
 function Title:update()
   self.title:update()
   self.anykey:update()
+  self.hormse_x += 1
+  if self.hormse_x > (flr(self.hormse_x / 20) * 20) then
+    self.hormse:set_front_hoof(self.hormse_x)
+  end
+  if self.hormse_x - 50 > (flr((self.hormse_x - 50) / 20) * 20) then
+    self.hormse:set_back_hoof(self.hormse_x - 50)
+  end
+  self.hormse:update()
   if peektext() then
     readtext()
     return true
@@ -35,6 +46,7 @@ end
 function Title:draw()
   cls()
   map(0,0)
+  self.hormse:draw()
   self.title:draw()
   self.anykey:draw()
 end
